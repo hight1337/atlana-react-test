cd14a795a07db0dc57357bd845d43a757c9a3834
Vlad <twister.09@mail.ru>
2021-10-31 10:03:56 +0200

    init commit

---
+++ src/pages/Repo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface RepoProps {}
+
+export const Repo: React.FC<RepoProps> = ({}) => {
+  return (
+    <div>
+      <p>Repo Page</p>
+    </div>
+  );
+};
---
+++ src/pages/User.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface UserProps {}
+
+export const User: React.FC<UserProps> = ({}) => {
+  return (
+    <div>
+      <p>UserPage</p>
+    </div>
+  );
+};
